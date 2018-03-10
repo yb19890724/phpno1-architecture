09a3da64d06b26e9e86347b19fc8c4a8b54bc2c3
hyb <354037968@qq.com>
2018-03-10 21:39:01 +0800

    驼峰命名空间

--- src/Generator/Commands/CreateResponse.php
+++ src/Generator/Commands/CreateResponse.php
@@ -56,7 +56,7 @@ class CreateResponse extends Command
     {
         return [
             'class_name'           => $this->name,
-            'namespace'            => $this->getFullNamespaceByType(static::COMMAND_KEY).'\\'.$this->name,
+            'namespace'            => $this->getFullNamespaceByType(static::COMMAND_KEY).'\\'.ucfirst($this->name),
         ];
     }
 }
